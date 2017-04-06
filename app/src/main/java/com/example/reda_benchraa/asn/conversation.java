1a31bd778ae785e9906f493f819ae1f9be2b6303
Reda Benchraa <reda.benchraa@um5s.net.ma>
2017-04-06 23:38:32 +0100

    JDBC

--- app/src/main/java/com/example/reda_benchraa/asn/conversation.java
+++ app/src/main/java/com/example/reda_benchraa/asn/conversation.java
@@ -4,6 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Toast;
+
+import com.mysql.jdbc.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class conversation extends AppCompatActivity {
     private Toolbar toolbar;                              // Declaring the Toolbar Object
@@ -14,7 +20,19 @@ public class conversation extends AppCompatActivity {
         setContentView(R.layout.activity_conversation);
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","root");
+            if(con == null){
+                Toast.makeText(this, "COnnection makhdamash", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "3la Slamtek", Toast.LENGTH_SHORT).show();
+            }
+        } catch (ClassNotFoundException | SQLException e1) {
+            e1.printStackTrace();
+        }
     }
+}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
