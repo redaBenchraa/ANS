7fa049dd39545fc2a96ee3bc3cbff423de030ae4
Reda Benchraa <reda.benchraa@um5s.net.ma>
2017-04-06 16:21:28 +0100

    Conversation

--- app/src/main/java/com/example/reda_benchraa/asn/group_poll.java
+++ app/src/main/java/com/example/reda_benchraa/asn/group_poll.java
@@ -2,12 +2,24 @@ package com.example.reda_benchraa.asn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class group_poll extends AppCompatActivity {
+    private Toolbar toolbar;                              // Declaring the Toolbar Object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_poll);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
 }
