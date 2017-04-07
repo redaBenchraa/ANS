72fc113ec7da56a20165e355d7513e85c875a9c3
Reda Benchraa <reda.benchraa@um5s.net.ma>
2017-04-07 22:51:34 +0100

    e

--- app/src/main/java/com/example/reda_benchraa/asn/conversation.java
+++ app/src/main/java/com/example/reda_benchraa/asn/conversation.java
@@ -15,7 +15,6 @@ import java.sql.SQLException;
 
 public class conversation extends AppCompatActivity {
     private Toolbar toolbar;                              // Declaring the Toolbar Object
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +25,6 @@ public class conversation extends AppCompatActivity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
