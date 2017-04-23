2f53cf00ab74cb7a87164a82a63bdfb91e3298fc
Reda Benchraa <reda.benchraa@um5s.net.ma>
2017-04-23 22:19:34 +0100

    Optimise

--- app/src/main/java/com/example/reda_benchraa/asn/Contacts.java
+++ app/src/main/java/com/example/reda_benchraa/asn/Contacts.java
@@ -1,19 +1,25 @@
 package com.example.reda_benchraa.asn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
 
 public class Contacts extends AppCompatActivity {
-    private Toolbar toolbar;                              // Declaring the Toolbar Object
-
+    private Toolbar toolbar;
+    ListView accountLv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        accountLv = (ListView) findViewById(R.id.listView_myContacts);
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -21,5 +27,31 @@ public class Contacts extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        switch (id){
+            case R.id.action_home:startActivity(new Intent(getApplicationContext(),home.class));break;
+            case R.id.action_myProfile:startActivity(new Intent(getApplicationContext(),MyProfile.class));break;
+            case R.id.action_myGroups:startActivity(new Intent(getApplicationContext(),MyGroups.class));break;
+            case R.id.action_myMessages:startActivity(new Intent(getApplicationContext(),MyMessages.class));break;
+            case R.id.action_myNotifications:startActivity(new Intent(getApplicationContext(),mynotification.class));break;
+            case R.id.action_myContacts:startActivity(new Intent(getApplicationContext(),Contacts.class));break;
+            case R.id.action_settings:startActivity(new Intent(getApplicationContext(),MyProfile.class));break;
+            case R.id.action_help:startActivity(new Intent(getApplicationContext(),MyProfile.class));break;
+            case R.id.action_signout:
+                SharedPreferences prefs = getSharedPreferences("PREFERENCE", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(getApplicationContext(),login.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
